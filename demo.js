7c0b4842dad47615156c10d8ce90c787ff3d7c20
AJ ONeal <aj@therootcompany.com>
2024-08-27 03:17:07 -0600

    wip: can generate and sign state transition

--- demo.js
+++ demo.js
@@ -5,15 +5,54 @@ let DashHd = require("dashhd");
 let DashKeys = require("dashkeys");
 let DashTx = require("dashtx");
 let DashPlatform = require("./dashplatform.js");
+let CBOR = require("CBOR");
 
 let KeyUtils = require("./key-utils.js");
 
 // let DapiGrpc = require("@dashevo/dapi-grpc");
-let Dpp = require("@dashevo/wasm-dpp");
+let WasmDpp = require("@dashevo/wasm-dpp");
+let Dpp = WasmDpp.DashPlatformProtocol;
+
+//@ts-ignore - sssssh, yes Base58 does exist
+let b58 = DashKeys.Base58.create();
 
 let rpcAuthUrl = "https://api:null@trpc.digitalcash.dev";
 
-const DIP13_ECDSA = 0;
+const L1_VERSION_PLATFORM = 3;
+const TYPE_ASSET_LOCK = 8;
+const L2_VERSION_PLATFORM = 1;
+const ST_CREATE_IDENTITY = 2;
+
+let KEY_LEVELS = {
+  0: "MASTER",
+  1: "CRITICAL",
+  2: "HIGH",
+  3: "MEDIUM",
+  MASTER: 0,
+  CRITICAL: 1,
+  HIGH: 2,
+  MEDIUM: 3,
+};
+
+let KEY_PURPOSES = {
+  0: "AUTHENTICATION",
+  1: "ENCRYPTION",
+  2: "DECRYPTION",
+  3: "TRANSFER",
+  4: "SYSTEM",
+  5: "VOTING",
+  AUTHENTICATION: 0,
+  ENCRYPTION: 1,
+  DECRYPTION: 2,
+  TRANSFER: 3,
+  SYSTEM: 4,
+  VOTING: 5,
+};
+
+let KEY_TYPES = {
+  0: "ECDSA_SECP256K1",
+  ECDSA_SECP256K1: 0,
+};
 
 let identityEcdsaPath = "";
 {
@@ -23,12 +62,16 @@ let identityEcdsaPath = "";
   let coinDash = 5;
   let featureId = 5;
   let subfeatureKey = 0;
-  let keyType = DIP13_ECDSA;
+  let keyType = KEY_TYPES.ECDSA_SECP256K1;
   identityEcdsaPath = `m/${purposeDip13}'/${coinDash}'/${featureId}'/${subfeatureKey}'/${keyType}'`;
 }
 
 async function main() {
+  void (await WasmDpp.default());
+
   let network = "testnet";
+
+  let dashTx = DashTx.create(KeyUtils);
 
   // let phrase = await DashPhrase.generate();
   let phrase =
@@ -83,10 +126,8 @@ async function main() {
     creditOutputs,
   });
 
-  const VERSION_PLATFORM = 3;
-  const TYPE_ASSET_LOCK = 8;
   let txDraft = {
-    version: VERSION_PLATFORM,
+    version: L1_VERSION_PLATFORM,
     type: TYPE_ASSET_LOCK,
     inputs: utxos,
     outputs: [burnOutput],
@@ -96,7 +137,10 @@ async function main() {
   console.log(`txDraft:`);
   console.log(txDraft);
 
-  let dashTx = DashTx.create(KeyUtils);
+  txDraft.inputs.sort(DashTx.sortInputs);
+  txDraft.outputs.sort(DashTx.sortOutputs);
+  let vout = txDraft.outputs.indexOf(burnOutput);
+
   let txSigned = await dashTx.hashAndSignAll(txDraft);
   console.log();
   console.log(`txSigned:`);
@@ -108,28 +152,131 @@ async function main() {
   //   txSigned.transaction,
   // );
 
-  // let assetLockProof = await YoureAWizardHarry.doMagic();
+  const INSTANT_ALP = 0;
+  const CHAIN_ALP = 1;
 
-  // let idIndex = 0; // increment to first unused
-  // await getKeysForIdentity(idIndex);
+  let blockchaininfo = await DashTx.utils.rpc(rpcAuthUrl, "getblockchaininfo");
+  let nextBlock = blockchaininfo.blocks + 1;
+
+  let fundingOutPointHex = await getFundingOutPointHex(
+    txSigned.transaction,
+    vout,
+  );
+  let identityId = createIdentityId(fundingOutPointHex);
+
+  /** @param {any} magicZmqEmitter */
+  async function getAssetLockInstantProof(magicZmqEmitter) {
+    let assetLockInstantProof = {
+      type: INSTANT_ALP,
+      instantLock: await magicZmqEmitter.once(
+        "zmqpubrawtxlocksig",
+        /** @param {any} instantLock */
+        function (instantLock) {
+          return instantLock.toBase64();
+        },
+      ),
+      transaction: txSigned.transaction,
+      outputIndex: vout,
+    };
+    return assetLockInstantProof;
+  }
+
+  async function getAssetLockChainProof() {
+    let assetLockChainProof = {
+      type: CHAIN_ALP,
+      coreChainLockedHeight: nextBlock,
+      outPoint: fundingOutPointHex,
+    };
+    return assetLockChainProof;
+  }
+
+  let assetLockProof;
+  let weEvenKnowHowToGetIsdlock = false;
+  if (weEvenKnowHowToGetIsdlock) {
+    assetLockProof = await getAssetLockInstantProof(null);
+  } else {
+    assetLockProof = await getAssetLockChainProof();
+  }
+
+  let idIndex = 0; // increment to first unused
+  let identityKeys = await getIdentityKeys(walletKey, idIndex);
+  let stKeys = await getIdentityTransitionKeys(identityKeys);
+
+  let stateTransition = {
+    protocolVersion: L2_VERSION_PLATFORM,
+    type: ST_CREATE_IDENTITY,
+    // ecdsaSig(assetLockPrivateKey, CBOR(thisStateTransition))
+    // "signature":"IBTTgge+/VDa/9+n2q3pb4tAqZYI48AX8X3H/uedRLH5dN8Ekh/sxRRQQS9LaOPwZSCVED6XIYD+vravF2dhYOE=",
+    assetLockProof: assetLockProof,
+    publicKeys: stKeys,
+    // [
+    //   {
+    //     id: 0,
+    //     type: 0,
+    //     purpose: 0,
+    //     securityLevel: 0,
+    //     data: "AkWRfl3DJiyyy6YPUDQnNx5KERRnR8CoTiFUvfdaYSDS",
+    //     readOnly: false,
+    //   },
+    // ],
+  };
+  console.log(`stKeys:`);
+  console.log(stKeys);
+
+  let cbor = CBOR.encodeCanonical(stateTransition);
+  console.log(`cbor:`);
+  console.log(DashTx.utils.bytesToHex(cbor));
+
+  let sigBytes = await KeyUtils.sign(addressKey.privateKey, cbor);
+  let sigHex = DashTx.utils.bytesToHex(sigBytes);
+  Object.assign(stateTransition, {
+    signature: sigHex,
+  });
+  console.log(JSON.stringify(stateTransition, null, 2));
 
   // let identityId = assetLockProof.createIdentifier();
   // let identity = Dpp.identity.create(identityId, dppKeys);
   // let signedTransition = signTransition(
-  //   idenity,
+  //   identity,
   //   assetLockProof,
   //   assetLockPrivateKeyBuffer,
   // );
 
   console.log("");
   console.log("TODO");
-  console.log(`  - which is considered the "asset lock private key buffer"?`);
-  console.log(`  - can we bypass Dpp.IdentityPublicKey?`);
-  console.log(`  - what's all this super signing the transition about?`);
-  console.log(`  - how to broadcast the transition?`);
+  console.log(`  - how to serialize and broadcast transition via grpc?`);
 }
 
-async function getKeysForIdentity(idIndex) {
+/**
+ * @param {Hex} txSignedHex
+ * @param {Uint32} outputIndex
+ */
+async function getFundingOutPointHex(txSignedHex, outputIndex) {
+  let txBytes = DashTx.utils.hexToBytes(txSignedHex);
+  let txidBytes = await DashTx.doubleSha256(txBytes);
+  let txidBE = DashTx.utils.bytesToHex(txidBytes);
+  let voutLE = DashTx.utils.toUint32LE(outputIndex);
+
+  let fundingOutPointHex = `${txidBE}${voutLE}`;
+  return fundingOutPointHex;
+}
+
+/**
+ * @param {Hex} fundingOutPointHex
+ */
+function createIdentityId(fundingOutPointHex) {
+  let fundingOutPointBytes = DashTx.utils.hexToBytes(fundingOutPointHex);
+  let identityHashBytes = DashTx.doubleSha256(fundingOutPointBytes);
+  let identityId = b58.encode(identityHashBytes);
+  return identityId;
+}
+
+/**
+ * @param {DashHd.HDKey} walletKey
+ * @param {Uint53} idIndex
+ * @returns {Promise<Array<EvoKey>>}
+ */
+async function getIdentityKeys(walletKey, idIndex) {
   let identityEcdsaKey = await DashHd.derivePath(walletKey, identityEcdsaPath);
   let identityKey = await DashHd.deriveChild(
     identityEcdsaKey,
@@ -137,63 +284,124 @@ async function getKeysForIdentity(idIndex) {
     DashHd.HARDENED,
   );
 
-  const MASTER_KEY = 0;
-  const HIGH_AUTH_KEY = 1;
-  const CRITICAL_KEY = 2;
-  const TRANSFER_KEY = 3;
-
   let keyDescs = [
     {
-      id: MASTER_KEY,
-      securityLevel: Dpp.IdentityPublicKey.SECURITY_LEVELS.MASTER,
+      id: 0,
+      type: KEY_TYPES.ECDSA_SECP256K1,
+      purpose: KEY_PURPOSES.AUTHENTICATION,
+      data: "",
+      securityLevel: KEY_LEVELS.MASTER,
+      // readOnly: false,
     },
     {
-      id: HIGH_AUTH_KEY,
-      securityLevel: Dpp.IdentityPublicKey.SECURITY_LEVELS.HIGH,
+      id: 1,
+      type: KEY_TYPES.ECDSA_SECP256K1,
+      purpose: KEY_PURPOSES.AUTHENTICATION,
+      data: "",
+      securityLevel: KEY_LEVELS.HIGH,
+      // readOnly: false,
     },
     {
-      id: CRITICAL_KEY,
-      securityLevel: Dpp.IdentityPublicKey.SECURITY_LEVELS.CRITICAL,
+      id: 2,
+      type: KEY_TYPES.ECDSA_SECP256K1,
+      purpose: KEY_PURPOSES.AUTHENTICATION,
+      data: "",
+      securityLevel: KEY_LEVELS.CRITICAL,
+      // readOnly: false,
     },
     {
-      id: TRANSFER_KEY,
-      purpose: Dpp.IdentityPublicKey.PURPOSES.TRANSFER,
-      securityLevel: Dpp.IdentityPublicKey.SECURITY_LEVELS.CRITICAL,
+      id: 3,
+      type: KEY_TYPES.ECDSA_SECP256K1,
+      purpose: KEY_PURPOSES.TRANSFER,
+      data: "",
+      securityLevel: KEY_LEVELS.CRITICAL,
+      // readOnly: false,
     },
   ];
 
-  let keys = [];
-  let dppKeys = [];
   for (let keyDesc of keyDescs) {
     let key = await DashHd.deriveChild(
       identityKey,
       keyDesc.id,
       DashHd.HARDENED,
     );
-    keys.push(key);
+    Object.assign(keyDesc, key);
 
-    let MAGIC_NUMBER_1 = 1; // TODO why?
-    let dppKey = new Dpp.IdentityPublicKey(MAGIC_NUMBER_1);
-    dppKey.setId(keyDesc.id);
-    dppKey.setData(key.publicKey);
-    if (keyDesc.purpose) {
-      dppKey.setPurpose(keyDesc.purpose);
-    }
-    dppKey.setSecurityLevel(keyDesc.securityLevel);
-    dppKeys.push(dppKey);
+    // let dppKey = new WasmDpp.IdentityPublicKey(L2_VERSION_PLATFORM);
+    // dppKey.setId(keyDesc.id);
+    // dppKey.setData(key.publicKey);
+    // if (keyDesc.purpose) {
+    //   dppKey.setPurpose(keyDesc.purpose);
+    // }
+    // dppKey.setSecurityLevel(keyDesc.securityLevel);
+    // dppKeys.push(dppKey);
   }
 
-  return dppKeys;
+  return keyDescs;
+}
+
+/**
+ * @typedef EvoKey
+ * @prop {Uint8} id
+ * @prop {Uint8} type - TODO constrain to members of KEY_TYPES
+ * @prop {Uint8} purpose - TODO constrain to members of KEY_PURPOSES
+ * @prop {Uint8} securityLevel - TODO constrain to members of KEY_LEVELS
+ * @prop {Boolean} readOnly
+ * @prop {Uint8Array} publicKey
+ * @prop {Uint8Array} privateKey
+ */
+
+/**
+ * @typedef STKey
+ * @prop {Uint8} id
+ * @prop {Uint8} type - TODO constrain to members of KEY_TYPES
+ * @prop {Uint8} purpose - TODO constrain to members of KEY_PURPOSES
+ * @prop {Base64} data - base64-encoded publicKey (compact)
+ * @prop {Uint8} securityLevel - TODO constrain to members of KEY_LEVELS
+ * @prop {Boolean} readOnly
+ */
+
+/**
+ * @param {Array<EvoKey>} identityKeys - TODO
+ */
+function getIdentityTransitionKeys(identityKeys) {
+  let stKeys = [];
+  for (let key of identityKeys) {
+    let data = bytesToBase64(key.publicKey);
+    let stKey = {
+      id: key.id,
+      type: key.type,
+      purpose: key.purpose,
+      securityLevel: key.securityLevel,
+      data: data,
+      // readOnly: key.readOnly,
+    };
+    if ("readOnly" in key) {
+      Object.assign(stKey, { readOnly: key.readOnly });
+    }
+    stKeys.push(stKey);
+  }
+  return stKeys;
+}
+
+/**
+ * @param {Uint8Array} bytes
+ */
+function bytesToBase64(bytes) {
+  let binstr = "";
+  for (let i = 0; i < bytes.length; i += 1) {
+    binstr += String.fromCharCode(bytes[i]);
+  }
+
+  return btoa(binstr);
 }
 
 function signTransition(identity, assetLockProof, assetLockPrivateKey) {
   // TODO is assetLockProof the same as txoutproof?
 
   // Create ST
-  const identityCreateTransition = Dpp.identity.createIdentityCreateTransition(
-    identity,
-    assetLockProof,
-  );
+  const identityCreateTransition =
+    WasmDpp.identity.createIdentityCreateTransition(identity, assetLockProof);
 
   // Create key proofs
   const [stMasterKey, stHighAuthKey, stCriticalAuthKey, stTransferKey] =
@@ -203,7 +411,7 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 
   identityCreateTransition.signByPrivateKey(
     identityMasterPrivateKey.toBuffer(),
-    IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+    Dpp.IdentityPublicKey.TYPES.ECDSA_SECP256K1,
   );
 
   stMasterKey.setSignature(identityCreateTransition.getSignature());
@@ -214,7 +422,7 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 
   identityCreateTransition.signByPrivateKey(
     identityHighAuthPrivateKey.toBuffer(),
-    IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+    Dpp.IdentityPublicKey.TYPES.ECDSA_SECP256K1,
   );
 
   stHighAuthKey.setSignature(identityCreateTransition.getSignature());
@@ -225,7 +433,7 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 
   identityCreateTransition.signByPrivateKey(
     identityCriticalAuthPrivateKey.toBuffer(),
-    IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+    Dpp.IdentityPublicKey.TYPES.ECDSA_SECP256K1,
   );
 
   stCriticalAuthKey.setSignature(identityCreateTransition.getSignature());
@@ -236,7 +444,7 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 
   identityCreateTransition.signByPrivateKey(
     identityTransferPrivateKey.toBuffer(),
-    IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+    Dpp.IdentityPublicKey.TYPES.ECDSA_SECP256K1,
   );
 
   stTransferKey.setSignature(identityCreateTransition.getSignature());
@@ -255,7 +463,7 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 
   identityCreateTransition.signByPrivateKey(
     assetLockPrivateKey,
-    IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+    Dpp.IdentityPublicKey.TYPES.ECDSA_SECP256K1,
   );
 
   // TODO(versioning): restore
@@ -276,3 +484,10 @@ function signTransition(identity, assetLockProof, assetLockPrivateKey) {
 }
 
 main();
+
+/** @typedef {String} Base58 */
+/** @typedef {String} Base64 */
+/** @typedef {String} Hex */
+/** @typedef {Number} Uint53 */
+/** @typedef {Number} Uint32 */
+/** @typedef {Number} Uint8 */
